6737e709dd418245ba83c6594ea4fe243417ff91
Contra <contra@maricopa.edu>
2013-11-22 08:04:30 -0700

    more event parsing

--- test/parseTracks.js
+++ test/parseTracks.js
@@ -13,7 +13,7 @@ var chopinPath = path.join(__dirname, './fixtures/chopin.mid');
 var chopin = fs.createReadStream(chopinPath);
 
 describe('parseTracks()', function() {
-  it('should parse a MIDI header correctly', function(done) {
+  it('should parse a MIDI track set correctly', function(done) {
     var parser = binary();
     parser.once('error', done);
 
@@ -22,11 +22,11 @@ describe('parseTracks()', function() {
 
     parser.tap(function(vars){
       should.exist(vars.tracks, 'should have created tracks');
+      should.exist(vars.tracks["0"], 'should have created track 0');
       should.exist(vars.tracks["1"], 'should have created track 1');
       should.exist(vars.tracks["2"], 'should have created track 2');
       should.exist(vars.tracks["3"], 'should have created track 3');
       should.exist(vars.tracks["4"], 'should have created track 4');
-      should.exist(vars.tracks["5"], 'should have created track 5');
 
       console.log(vars);
       done();
