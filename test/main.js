ba5260430b977082dfe78690fc31e4edfdb5ec9a
Contra <contra@maricopa.edu>
2013-11-25 16:47:29 -0700

    hack to finish this thing

--- test/main.js
+++ test/main.js
@@ -4,15 +4,6 @@ require('mocha');
 var Dissolve = require('dissolve');
 var midistream = require('../');
 
-Dissolve.prototype.autoRead = function(){
-  this.on("readable", function() {
-    var e;
-    while (e = this.read()) {
-      console.log(e);
-    }
-  }.bind(this));
-};
-
 describe('createStream()', function() {
   it('should create a writeable stream', function(done) {
     var stream = midistream.createStream();
